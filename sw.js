0205364ee4461c1bd47bfd79b6ac1e8a6e230841
sylvainmercier5 <sylvainmercier5@googlemail.com>
2026-07-15 10:35:48 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // BioDex v1.24 — Service Worker
 // © 2026 Sylvain Mercier. Tous droits réservés.
-const CACHE = "biodex-v1-26";
+const CACHE = "biodex-v1-27";
 const SHELL = [
   "./",
   "./index.html",
